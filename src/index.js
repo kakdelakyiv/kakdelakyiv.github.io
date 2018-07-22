09393128c97476ad114d4a14063842f9fafc1a42
Bosko <bosko.kovacevic@gmail.com>
2018-07-22 23:29:53 +0200

    dfg

--- src/index.js
+++ src/index.js
@@ -43,13 +43,31 @@ window.onload = function () {
     topics.push(new Topic(lon + 0.09, lat + 0.0136, 'yasia-khomenko', 'Yasia Khomenko', 'Something'));
     topics.push(new Topic(lon + 0.1, lat + 0.0137, 'vova-vorotniov', 'Vova Vorotniov', 'Something'));
     topics.push(new Topic(lon + 0.11, lat + 0.0138, 'evgenya-vidischeva', 'Evgenya Vidischeva', 'Something'));
-    
+
 
     const modal = new Modal();
     let map;
+    let teaserVidContent = '';
     if (!isMobile()) {
         map = new mapboxgl.Map(options);
+
+
+        teaserVidContent = `<video playsinline="true" autoplay="autoplay" muted loop width="100%">
+            <source src="vid/landing-page/video.webm" type="video/webm">
+            <source src="vid/landing-page/video.ogv" type="video/ogv">
+            <source src="vid/landing-page/video.mp4" type="video/mp4"> Sorry, your browser does not support HTML5 videos.
+        </video>
+        <div id="video-arrow" class="arrow bounce"></div>`;
+
+
+    } else {
+        teaserVidContent = '<img src="img/kyiv.jpg" style="width:100%;">';
     }
+
+    const teaserContainer = document.getElementById('teaser-container');
+    teaserContainer.innerHTML = teaserVidContent;
+
+
     const grid = new Grid(modal);
 
     topics.forEach(topic => {
@@ -64,17 +82,17 @@ window.onload = function () {
         grid.addCell(topic);
     });
 
-    function removeFadeOut( el, speed ) {
-        var seconds = speed/1000;
-        el.style.transition = "opacity "+seconds+"s ease";
+    function removeFadeOut(el, speed) {
+        var seconds = speed / 1000;
+        el.style.transition = "opacity " + seconds + "s ease";
         el.style.opacity = 1;
     }
-    
+
     removeFadeOut(document.getElementById('main-title'), 4000);
 
     const aboutUsLink = document.getElementById('about-us');
 
-    aboutUsLink.addEventListener('click', function() {
+    aboutUsLink.addEventListener('click', function () {
         modal.show(null, 'html/about-us.html', null);
     });
 };
@@ -82,7 +100,7 @@ window.onload = function () {
 var mainTitleContainer, mainTitleH1, mainTitleH4;
 var lastCall = 0;
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     if (!mainTitleContainer || !mainTitleH1 || !mainTitleH4) {
         mainTitleContainer = document.getElementById('main-title');
         mainTitleH1 = document.getElementById('main-title-h1');
@@ -98,9 +116,9 @@ window.addEventListener('scroll', function() {
 
     lastCall = now;
     const scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
-        
-    const mainTitleTop = Math.min(20, 0.1*scrollPos) + 'vh'; // between 0vh to 80vh
-    const mainTitleSize = Math.max(8, 10 - 0.005*scrollPos) + 'vw'; // between 10vw to 6vw
+
+    const mainTitleTop = Math.min(20, 0.1 * scrollPos) + 'vh'; // between 0vh to 80vh
+    const mainTitleSize = Math.max(8, 10 - 0.005 * scrollPos) + 'vw'; // between 10vw to 6vw
     // const subTitleSize = Math.max(2, 3 - 0.005*scrollPos) + 'vw'; // between 3vw to 1vw
 
     mainTitleH1.style.fontSize = mainTitleSize;
